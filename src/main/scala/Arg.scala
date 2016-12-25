c0c0f9221a79ddf61f4cafd4e7de167f7812c220
davips <davips>
2016-12-25 14:39:06 -0200

    pronto

--- src/main/scala/Arg.scala
+++ src/main/scala/Arg.scala
@@ -1,7 +1,40 @@
+import java.io.Serializable
+
+import scala.language.postfixOps
+
 trait Arg extends App {
-  val startsOfList: Seq[String]
-  lazy val argsbool = args filter (x => x.endsWith("=y") || x.endsWith("=n"))
-  lazy val argsnumb = args filter (x => x.split('=').last.filter(x => x != '.' && x != '-').forall(x => x.isDigit))
-  lazy val argslist = args filter (x => x.contains(",") || startsOfList.exists(start => x.startsWith(start + "=")))
-  lazy val argstext = args diff argsbool diff argsnumb diff argslist
+  private lazy val argsbool0 = args filter (x => x.endsWith("=y") || x.endsWith("=n"))
+  private lazy val argsnumb0 = args filter (x => x.split('=').last.filter(x => x != '.' && x != '-').forall(x => x.isDigit))
+  private lazy val argslist0 = args filter (x => x.contains(","))
+  private lazy val argstext0 = args diff argsbool0 diff argsnumb0 diff argslist0
+
+  lazy val argsall = args map parse toMap
+  lazy val argsbool1 = argsbool0 map parse toMap
+  lazy val argsnumb1 = argsnumb0 map parse toMap
+  lazy val argslist1 = argsall.toList map { case (k, v) => k -> v.split(',').toList } toMap
+  lazy val argstext1 = argstext0 map parse toMap
+
+  lazy val consistent = args.length == (args map parse).map(_._1).distinct.length
+
+  def catchError[T](argsmap: Map[String, T], k: String): T = if (consistent) argsmap.getOrElse(k, {
+    if (argsall.contains(k)) println(s"Invalid value '${argsall(k)}' for argument '$k'.")
+    else println(s"Missing argument '$k'.")
+    sys.exit(0)
+  }) else {
+    println(s"Inconsistent list of arguments '${args.mkString("\n")}'.")
+    sys.exit(0)
+  }
+
+  def argsbool(k: String): String = catchError(argsbool1, k)
+
+  def argsnumb(k: String): String = catchError(argsnumb1, k)
+
+  def argslist(k: String): Serializable = catchError(argslist1, k)
+
+  def argstext(k: String): String = catchError(argstext1, k)
+
+  def parse(s: String): (String, String) = {
+    val Seq(a, b) = s.split('=').toSeq
+    a -> b
+  }
 }
