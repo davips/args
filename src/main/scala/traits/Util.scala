0663ba96b8b20aebcb69e41aaabd752e34e428d3
davips <dpsabc@gmail.com>
2017-01-10 18:04:34 -0200

    overlay

--- src/main/scala/traits/Util.scala
+++ src/main/scala/traits/Util.scala
@@ -5,13 +5,21 @@ import java.io.{File, PrintWriter}
 import scala.io.Source
 
 trait Util {
-  def replaceInFile(arq: String, before: String, after: String, content:String): Unit = {
+  def replaceInFile(arq: String, before: String, after: String, content: String): Unit = {
     val f2 = new File("/run/shm/lixo.tmp")
     val w = new PrintWriter(f2)
     Source.fromFile(arq).getLines.map {
       case x if x.contains(before) && x.contains(after) => x.replaceAll(s"$before.*$after", s"$before$content$after")
       case x => x
     }.foreach(x => w.println(x))
+    w.close()
+    f2.renameTo(new File(arq))
+  }
+
+  def replaceInFile(arq: String, old: String, neww: String): Unit = {
+    val f2 = new File("/run/shm/lixo.tmp")
+    val w = new PrintWriter(f2)
+    Source.fromFile(arq).getLines.mkString("\n").replace(old, neww).split("\n").foreach(x => w.println(x))
     w.close()
     f2.renameTo(new File(arq))
   }
