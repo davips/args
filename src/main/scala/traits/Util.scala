a8812998813f9d6320c236424104b7b08062ce84
davips <dpsabc@gmail.com>
2017-01-12 16:37:39 -0200

    pronto

--- src/main/scala/traits/Util.scala
+++ src/main/scala/traits/Util.scala
@@ -1,26 +1,24 @@
 package traits
 
-import java.io.{File, PrintWriter}
+import java.io.{File, FileWriter}
 
 import scala.io.Source
 
 trait Util {
   def replaceInFile(arq: String, before: String, after: String, content: String): Unit = {
-    val f2 = new File("/run/shm/lixo.tmp")
-    val w = new PrintWriter(f2)
+    val fw = new FileWriter(s"$arq.tmp")
     Source.fromFile(arq).getLines.map {
       case x if x.contains(before) && x.contains(after) => x.replaceAll(s"$before.*$after", s"$before$content$after")
       case x => x
-    }.foreach(x => w.println(x))
-    w.close()
-    f2.renameTo(new File(arq))
+    }.foreach(x => fw.write(s"$x\n"))
+    fw.close()
+    new File(s"$arq.tmp").renameTo(new File(arq))
   }
 
   def replaceInFile(arq: String, old: String, neww: String): Unit = {
-    val f2 = new File("/run/shm/lixo.tmp")
-    val w = new PrintWriter(f2)
-    Source.fromFile(arq).getLines.mkString("\n").replace(old, neww).split("\n").foreach(x => w.println(x))
-    w.close()
-    f2.renameTo(new File(arq))
+    val fw = new FileWriter(s"$arq.tmp")
+    Source.fromFile(arq).getLines.mkString("\n").replace(old, neww).split("\n").foreach(x => fw.write(s"$x\n"))
+    fw.close()
+    new File(s"$arq.tmp").renameTo(new File(arq))
   }
 }
